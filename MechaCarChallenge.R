fede9ffd283411c4402c84a6ae4bcd07ea6fb6b3
Teresa Hartman <teresa2h@users.campus>
2022-11-12 18:31:12 -0600

    Done with analysis for Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,15 @@ lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD,data=car_data_t
 
 #get summary of linear regression model 
 summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD,data=car_data_table))
+
+#import MechaCar dataset
+sup_coil_table <- read.csv('Resources/Suspension_Coil.csv',check.names = F,stringsAsFactors = F) 
+
+#Getting mean, median, variance, sd of PSI and putting into dataframe
+total_summary <- sup_coil_table %>% summarize(Mean =mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI) )
+
+#Summarizing data and grouping by lot information 
+lot_summary<- sup_coil_table %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean =mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') 
+
+
